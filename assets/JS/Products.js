3318a2f38b9b41ff22630eb6bd01c5c58e185e80
mernaelnashar <mernaelnashar18@gmail.com>
2026-03-01 02:54:55 +0200

    Enhance navbar responsiveness and styling; add toast notifications for wishlist actions

--- assets/JS/Products.js
+++ assets/JS/Products.js
@@ -1,19 +1,37 @@
 document.querySelectorAll('.btn-add-wishlist').forEach(button => {
-    button.addEventListener('click', function() {
+    button.addEventListener('click', function () {
         this.classList.toggle('active');
-        
+
         const icon = this.querySelector('i');
         if (this.classList.contains('active')) {
             icon.classList.replace('fa-regular', 'fa-solid');
+            Swal.fire({
+                toast: true,
+                position: 'top-end',
+                icon: 'success',
+                title: 'Added to your wishlist ❤️',
+                showConfirmButton: false,
+                timer: 1500,
+                timerProgressBar: true
+            });
         } else {
             icon.classList.replace('fa-solid', 'fa-regular');
+            Swal.fire({
+                toast: true,
+                position: 'top-end',
+                icon: 'warning',
+                title: 'Removed from your wishlist',
+                showConfirmButton: false,
+                timer: 1500,
+                timerProgressBar: true
+            });
         }
     });
 });
 
 
 document.querySelectorAll('.btn-add-cart').forEach(button => {
-    button.addEventListener('click', function() {
+    button.addEventListener('click', function () {
         Swal.fire({
             toast: true,
             position: 'top-end',
@@ -26,16 +44,3 @@ document.querySelectorAll('.btn-add-cart').forEach(button => {
     });
 });
 
-document.querySelectorAll('.btn-add-wishlist').forEach(button => {
-    button.addEventListener('click', function() {
-        Swal.fire({
-            toast: true,
-            position: 'top-end',
-            icon: 'success',
-            title: 'Add to your wishlist ❤️',
-            showConfirmButton: false,
-            timer: 2000,
-            timerProgressBar: true
-        });
-    });
-});
